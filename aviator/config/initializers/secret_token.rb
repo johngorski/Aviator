085972c913cbb591a43606684d9f798a2d4db4cb
John Gorski <john.gorski.jr@gmail.com>
2014-02-11 20:54:39 -0800

    rails generate aviator

---
+++ aviator/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aviator::Application.config.secret_token = '0d6457c2466db76f41bff703f8a8f6291b1934ee5d7cb83b605607cd13345f0c212ed6a2dd48b074e68e0ae1567394e5125d9c02d2f261447a6dc2176c19a9d5'
